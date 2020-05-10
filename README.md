6fdcd0cf6dba76e7b7d1914dc500c86018703887
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-05-10 13:38:33 -0300

    Node.js Boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # heroku-templates
  
+Boilerplate for a [Node.js](https://nodejs.org/dist/latest-v12.x/docs/api/) based app, deploy easily on Heroku.
+
+## Deploy (Heroku)
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy "Deploy to Heroku")
---
+++ server.js
@@ -0,0 +1,17 @@
+
+console.log('The result is displayed in the Command Line Interface');
+
+const http = require('http');
+
+const hostname = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

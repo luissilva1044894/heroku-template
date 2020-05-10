51ddc85446aa69b5c5ddc7da2d5c86a0ca4554a7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-05-10 13:45:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ console.log('The result is displayed in the Command Line Interface');
 const http = require('http');
 
 const hostname = process.env.HOST || '127.0.0.1';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,6 +12,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port/*, hostname*/, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });

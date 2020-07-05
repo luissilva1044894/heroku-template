c96bbcb4c71c62d440df2b813e83345dde758b7a
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-07-05 15:00:59 -0300

    Some fixes

--- discord_bot.js
+++ discord_bot.js
@@ -1,16 +1,17 @@
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+const bot_prefix = process.env.DISCORD_BOT_PREFIX || '!';
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('Deployed on Heroku', {type: 'PLAYING'});
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+  bot.user.setActivity(`${bot.user.username} is Deployed on Heroku!`, {type: 'PLAYING'});
 });
 
-client.on('message', msg => {
-	if (!msg.content.startsWith(process.env.DISCORD_BOT_PREFIX))
+bot.on('message', msg => {
+	if (!msg.content.startsWith(bot_prefix))
 		return;
-  const command = msg.content.split(' ')[0].substr(process.env.DISCORD_BOT_PREFIX.length);
+  const command = msg.content.split(' ')[0].substr(bot_prefix.length);
   const args = msg.content.split(' ').slice(1).join(' ');
   if (command === 'ping') {
     msg.reply('Pong!');
@@ -19,4 +20,4 @@ client.on('message', msg => {
   }
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN); //Bot Token
+bot.login(process.env.DISCORD_BOT_TOKEN); //Bot Token

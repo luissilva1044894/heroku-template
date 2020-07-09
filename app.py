321c77026066c40ad46279f26dc7e0aa151da58a
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-07-09 18:22:03 -0300

    Some changes

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from functools import wraps
 import os
 
-from flask import Flask, jsonify, request
-from twilio.twiml.messaging_response import MessagingResponse
+from flask import (
+    abort,
+    current_app,
+    Flask,
+    jsonify,
+    request,
+)
+from twilio.request_validator import RequestValidator
+try:
+    from twilio.twiml import Response
+except ImportError:
+    from twilio.twiml.messaging_response import MessagingResponse as Response
 
 app = Flask(__name__)
+
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid (or if DEBUG is True)
+        # and return a 403 error if it's not
+        if request_valid or current_app.debug:
+            return f(*args, **kwargs)
+        return abort(403)
+    return decorated_function
 
 # Sample main page
 @app.route('/')
@@ -28,15 +60,45 @@ def setup_route():
     }
     return jsonify(data)
 
-@app.route('/reply/', methods=['POST'], strict_slashes=False)
-def reply_route():
-    """Respond to incoming calls with a simple text message."""
+@app.route('/voice/', methods=['POST'], strict_slashes=False)
+@validate_twilio_request
+def incoming_call():
+    """Twilio Voice URL - receives incoming calls from Twilio"""
 
-    msg = request.form.get('Body')
+    # Create a new TwiML response
+    resp = Response()
 
-    resp = MessagingResponse()
-    resp.message(f'Your message: {msg}')
+    # <Say> a message to the caller
+    from_number = request.values['From']
+    body = """
+    Thanks for calling!
 
+    Your phone number is {0}. I got your call because of Twilio's webhook.
+
+    Goodbye!""".format(' '.join(from_number))
+    resp.say(body)
+
+    # Return the TwiML
+    return str(resp)
+
+@app.route('/message/', methods=['POST'], strict_slashes=False)
+@validate_twilio_request
+def incoming_message():
+    """Twilio Messaging URL - Respond to incoming messages from Twilio with a simple text message"""
+
+    # Create a new TwiML response
+    resp = Response()
+
+    # <Message> a text back to the person who texted us
+    msg = resp.message()
+
+    # retrieve incoming message from POST request
+    incoming_msg = request.POST['Body']
+
+    msg.body(f"Your text to me was {incoming_msg} characters long. Webhooks are neat :)")
+    msg.media('https://twilio-cms-prod.s3.amazonaws.com/images/twilio-logo-red.width-640.png')
+
+    # Return the TwiML
     return resp.to_xml()
 
 if __name__ == '__main__':
